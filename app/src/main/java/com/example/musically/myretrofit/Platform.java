32fe0b5382a239706bca83ab239a38dc0881e2ba
wang.lei <wang.lei@bytedance.com>
2019-10-17 14:45:47 +0800

    fix:更新platform

--- app/src/main/java/com/example/musically/myretrofit/Platform.java
+++ app/src/main/java/com/example/musically/myretrofit/Platform.java
@@ -1,5 +1,7 @@
 package com.example.musically.myretrofit;
 
+import android.os.Build;
+
 /**
  * <pre>
  *     author : 王磊
@@ -12,7 +14,15 @@ public class Platform {
     private static final Platform PLATFORM = findPlatform();
 
     private static Platform findPlatform() {
+        try {
+            Class.forName("android.os.Build");
+            if (Build.VERSION.SDK_INT != 0) {
+                return new Android();
+            }
+        } catch (ClassNotFoundException ignored) {
 
+        }
+        return new Platform();
     }
 
     public static Platform get() {
